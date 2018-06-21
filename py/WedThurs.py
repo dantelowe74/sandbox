e032f842076603e1e1307022a1686f322283b669
dantelowe74 <39770666+dantelowe74@users.noreply.github.com>
2018-06-21 13:13:32 -0400

    Create WedThurs.py

---
+++ py/WedThurs.py
@@ -0,0 +1,147 @@
+>>> import time
+>>> time.time()
+1529505815.5660107
+>>> 
+================ RESTART: C:/Users/linda/Desktop/wednesday.py ================
+>>> 
+================ RESTART: C:/Users/linda/Desktop/wednesday.py ================
+>>> countdown(3)
+3
+2
+1
+blastoff
+>>> countdown(2)
+2
+1
+blastoff
+>>> 
+================ RESTART: C:/Users/linda/Desktop/wednesday.py ================
+>>> print_n(2, 4)
+2
+2
+2
+2
+>>> print_n(2, 0)
+>>> print(2)
+2
+>>> math
+Traceback (most recent call last):
+  File "<pyshell#28>", line 1, in <module>
+    math
+NameError: name 'math' is not defined
+>>> import math
+>>> 
+================ RESTART: C:\Users\linda\Desktop\wednesday.py ================
+>>> area(2.3)
+Traceback (most recent call last):
+  File "<pyshell#30>", line 1, in <module>
+    area(2.3)
+  File "C:\Users\linda\Desktop\wednesday.py", line 2, in area
+    a = math.pi * radius**2
+NameError: name 'math' is not defined
+>>> import math
+>>> 
+================ RESTART: C:\Users\linda\Desktop\wednesday.py ================
+>>> area(2.5)
+19.634954084936208
+>>> e = math.exp(1.0)
+>>> height = radius * math.sin(radius)
+Traceback (most recent call last):
+  File "<pyshell#34>", line 1, in <module>
+    height = radius * math.sin(radius)
+NameError: name 'radius' is not defined
+>>> 
+================ RESTART: C:\Users\linda\Desktop\wednesday.py ================
+>>> absolute_value(2-4)
+2
+>>> absolute_value(-2)
+2
+>>> absolute_value(0)
+0
+>>> 
+================ RESTART: C:\Users\linda\Desktop\wednesday.py ================
+>>> absolute_value(0)
+>>> print(absolute_value(0))
+None
+>>> print(absolute_value(1))
+1
+>>> 
+================ RESTART: C:\Users\linda\Desktop\wednesday.py ================
+>>> erercise(3, 3)
+Traceback (most recent call last):
+  File "<pyshell#41>", line 1, in <module>
+    erercise(3, 3)
+NameError: name 'erercise' is not defined
+>>> 
+================ RESTART: C:\Users\linda\Desktop\wednesday.py ================
+>>> 
+================ RESTART: C:\Users\linda\Desktop\wednesday.py ================
+>>> compare(3, 3)
+0
+>>> 
+================ RESTART: C:\Users\linda\Desktop\wednesday.py ================
+>>> distance(x1)
+Traceback (most recent call last):
+  File "<pyshell#43>", line 1, in <module>
+    distance(x1)
+NameError: name 'distance' is not defined
+>>> distance(x1)
+Traceback (most recent call last):
+  File "<pyshell#44>", line 1, in <module>
+    distance(x1)
+NameError: name 'distance' is not defined
+>>> 
+================ RESTART: C:\Users\linda\Desktop\wednesday.py ================
+>>> distance(x1)
+Traceback (most recent call last):
+  File "<pyshell#45>", line 1, in <module>
+    distance(x1)
+NameError: name 'x1' is not defined
+>>> distance(1, 2, 4, 5)
+0.0
+>>> 
+================ RESTART: C:\Users\linda\Desktop\wednesday.py ================
+>>> distance(1, 2, 4, 6)
+dx is 3
+dy is 4
+0.0
+>>> 
+================ RESTART: C:\Users\linda\Desktop\wednesday.py ================
+>>> distance(1, 2, 4, 6)
+dsquared is:  25
+0.0
+>>> 
+================ RESTART: C:\Users\linda\Desktop\wednesday.py ================
+>>> distance(1, 2, 4, 6)
+dsquared is:  25
+Traceback (most recent call last):
+  File "<pyshell#49>", line 1, in <module>
+    distance(1, 2, 4, 6)
+  File "C:\Users\linda\Desktop\wednesday.py", line 29, in distance
+    return result
+NameError: name 'result' is not defined
+>>> 
+================ RESTART: C:\Users\linda\Desktop\wednesday.py ================
+>>> distance(1, 2, 4, 6)
+5.0
+>>> 
+================ RESTART: C:\Users\linda\Desktop\wednesday.py ================
+>>> circle_area(2, 3, 4, 5)
+25.132741228718352
+>>> 
+================ RESTART: C:\Users\linda\Desktop\wednesday.py ================
+>>> circle_area(2, 3, 4, 5)
+25.132741228718352
+>>> 
+================ RESTART: C:\Users\linda\Desktop\wednesday.py ================
+>>> is divisible(6, 4)
+SyntaxError: invalid syntax
+>>> is_divisible(6, 4)
+False
+>>> 
+================ RESTART: C:\Users\linda\Desktop\wednesday.py ================
+>>> factorial(3)
+6
+>>> factorial(0)
+1
+>>> 
